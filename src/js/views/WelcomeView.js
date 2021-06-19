2fcfa0b7aee657d16328ee751db4e8bf3e310a9e
Gaurav Gupta <gouravgupta840@gmail.com>
2021-06-19 23:18:23 +0530

    toggle between forms

--- src/js/views/WelcomeView.js
+++ src/js/views/WelcomeView.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LoginForm from './../components/LoginForm';
 import RegisterForm from './../components/RegisterForm';
 
 export default function WelcomeView() {
+
+    const [isLoginView, setIsLogin] = useState(true);
+    const optInText = isLoginView ?
+        ['Need an account?', 'Register'] :
+        ['Already Registered?', 'Login']
+
     return (
         <div className="centered-view">
             <div className="centered-container">
-                <LoginForm />
-                <small className="form-text text-muted mt-2">Already registered?
+                {isLoginView ? <LoginForm /> : <RegisterForm />}
+                <small className="form-text text-muted mt-2">{optInText[0]}
                     <span
-                        onClick={() => { }}
-                        className="btn-link ml-2">Login</span></small>
+                        onClick={() => setIsLogin(!isLoginView)}
+                        className="btn-link ml-2">{optInText[1]}</span></small>
             </div>
         </div>
     )
