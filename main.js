b006ee419f64ab229f3cdca5b8e1e47f99e95466
Gaurav Gupta <gouravgupta840@gmail.com>
2021-06-19 11:47:44 +0530

    Create browser basic window

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('hello, world');
+const { app, BrowserWindow, Menu } = require('electron')
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 800,
+        backgroundColor: 'white',
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+    const menuTemplate = Menu.buildFromTemplate([{label: 'File', submenu: [ { label: 'Exit', click: () => { app.exit() } } ]}])
+    Menu.setApplicationMenu(menuTemplate);
+}
+
+app.whenReady().then(createWindow)
+app.on('ready', (e) => {})
