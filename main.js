b8deac766ddeef6b250c1c21c48357ff95c3a4a6
Gaurav Gupta <gouravgupta840@gmail.com>
2021-06-19 11:59:47 +0530

    added basic event hook for listener

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let mainWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({
-        width: 800,
+        width: 1200,
         height: 800,
         backgroundColor: 'white',
         webPreferences: {
@@ -12,11 +12,25 @@ function createWindow() {
         }
     })
 
+    mainWindow.webContents.openDevTools()
     mainWindow.loadURL(`file://${__dirname}/index.html`)
 
     const menuTemplate = Menu.buildFromTemplate([{label: 'File', submenu: [ { label: 'Exit', click: () => { app.exit() } } ]}])
     Menu.setApplicationMenu(menuTemplate);
+
+    mainWindow.on('closed', () => {mainWindow = null});
 }
 
 app.whenReady().then(createWindow)
 app.on('ready', (e) => {})
+app.on('window-all-closed', (e) => {
+    if(process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+// only for mac os where application hangs in dock
+app.on('activate', (e) => {
+    if(BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+})
