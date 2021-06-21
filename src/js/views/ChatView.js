0d62a356338fe30d7368222dce2615cb1efab7a2
Gaurav Gupta <gouravgupta840@gmail.com>
2021-06-21 16:08:11 +0530

    Unsubsubscribing people which we are listing while chating

--- src/js/views/ChatView.js
+++ src/js/views/ChatView.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { withBaseLayout } from '../Hoc/BaseLayout';
@@ -12,13 +12,15 @@ import { subscribeToChat, subscribeToProfile } from '../actions/chats-actions';
 function ChatView() {
     const { id } = useParams()
     const dispatch = useDispatch()
-    const activeChat = useSelector(({chats}) => chats.activeChat[id])
+    const watchingPeople = useRef({}); // this will presist the value even if re-render happens, we just use 'watchingPeople.current'
+    const activeChat = useSelector(({ chats }) => chats.activeChat[id])
     const joinedUsers = activeChat?.joinedUsers
 
     useEffect(() => {
         const unsubscribeChat = dispatch(subscribeToChat(id))
         return () => {
             unsubscribeChat()
+            unsubscribeWatchedPeoples()
         }
     }, [])
 
@@ -27,9 +29,21 @@ function ChatView() {
         joinedUsers && subscribedToJoinedUsers(joinedUsers)
     }, [joinedUsers]);
 
-
     const subscribedToJoinedUsers = jUsers => {
-        jUsers.forEach(user => dispatch(subscribeToProfile(user.uid)));
+        jUsers.forEach(user => {
+            if (!watchingPeople.current[user.uid]) {
+                watchingPeople.current[user.uid] = dispatch(subscribeToProfile(user.uid))
+            }
+        });
+    }
+
+    const unsubscribeWatchedPeoples = () => {
+        // has dispatch inside watchingPeople.current[x] so we just need to call
+        Object.keys(watchingPeople.current).forEach(id => {
+            // watchingPeople.current[id](); <- will work as well
+            const unsubscribeMe = watchingPeople.current[id];
+            unsubscribeMe();
+        })
     }
 
     return (
@@ -45,4 +59,4 @@ function ChatView() {
     )
 }
 
-export default withBaseLayout(ChatView, {canGoBack: true})
+export default withBaseLayout(ChatView, { canGoBack: true })
