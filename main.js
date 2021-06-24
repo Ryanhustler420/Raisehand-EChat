b98aab1d2a6a209b313aa61a21f13d4f19e5feea
Gaurav Gupta <gouravgupta840@gmail.com>
2021-06-24 11:10:42 +0530

    Multiple windows demonstration

--- main.js
+++ main.js
@@ -4,6 +4,23 @@ const { app, BrowserWindow, Menu, Notification, ipcMain } = require('electron')
 const isDevelopment = !app.isPackaged
 let mainWindow;
 
+// function createSecondWindow() {
+//     let sec = new BrowserWindow({
+//         width: 1200,
+//         height: 800,
+//         backgroundColor: '#6e707e',
+//         webPreferences: {
+//             nodeIntegration: false, // prevent html to access the ipcRenderer, so that they can't missuse these function
+//             contextIsolation: true, // cant override preload file values via console of browser
+//             enableRemoteModule: true, // Allow renderer to access Electron Native API which only get access in main thread
+//             worldSafeExecuteJavaScript: true, // Sanitize JS code
+//         }
+//     })
+
+//     sec.loadURL(`file://${__dirname}/second.html`)
+//     sec.on('closed', () => { sec = null });
+// }
+
 function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1200,
@@ -37,6 +54,7 @@ app.whenReady().then(() => {
     const menu = Menu.buildFromTemplate(template);
     Menu.setApplicationMenu(menu)
     createWindow()
+    // createSecondWindow()
 })
 
 app.on('ready', (e) => { })
